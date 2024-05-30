b330f09e6b007c5ffec0c80c7349af57cedfad4a
Bychko Kirilo <109760434+StraNgerik@users.noreply.github.com>
2024-05-30 20:40:18 +0300

    Add files via upload

---
+++ front/src/pages/about/about.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<p class="h1 text-uppercase calculator-about-logo text-center ">
+					 Calculator for programmers
+		</p>
+				
+		<span class="text-left calculator-about-logo-subtext h4">
+		Calculator for programmers, made by Bychko Kyrylo FPM KB-12.
+		</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'about',
+		data() {
+			return {
+			}
+		},
+		methods: {
+		},
+		mounted() {
+			
+		},
+	}
+</script>
+
+<style>
+	
+</style>
